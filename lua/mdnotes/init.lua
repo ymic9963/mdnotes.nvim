ca164d8ad7f08655d94660ce9abd61c821fa01a4
ymich9963 <gmichael834@gmail.com>
2026-09-20 01:04:11 +0300

    fix: get_fragment_from_destination() move to search term

--- lua/mdnotes/init.lua
+++ lua/mdnotes/init.lua
@@ -1004,10 +1004,12 @@ function M.get_fragment_from_destination(destination, check_valid, opts)
                 return fragment, -3, "fragment not parsed"
             end
 
+            local cur_pos = vim.fn.getpos(".")
             local search_ret = 0
             vim.api.nvim_buf_call(buf, function()
                 search_ret = vim.fn.search("# " .. new_fragment)
             end)
+            vim.fn.setpos(".", cur_pos)
 
             if search_ret == 0 then
                 return fragment, -4, "invalid fragment: ".. new_fragment
